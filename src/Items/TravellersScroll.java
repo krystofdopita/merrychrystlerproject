fc83e4d1fdfa3ca450c5102fd47f53a5d967ea91
krystofdopita <krystofkolumbus99@gmail.com>
2026-01-13 16:19:02 +0100

    hlavicky metod

--- src/Items/TravellersScroll.java
+++ src/Items/TravellersScroll.java
@@ -4,7 +4,7 @@ public class TravellersScroll {
 
     //private String mapTS = "[ HRAD ]──[ LES ]──[ VESNIČAN ]──[ OBCHODNÍK ]──[ POUTNÍK ]──[ PEVNOST ]";
     public String getMapTS(){
-
+    return "a";
     }
 
 }
